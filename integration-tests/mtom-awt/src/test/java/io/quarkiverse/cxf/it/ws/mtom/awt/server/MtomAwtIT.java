9263fab7f83c582de625cd8968c329aab68d390e
Shumon Sharif <shumon.sharif@gmail.com>
2022-12-02 09:02:13 -0500

    Fix MTOM/MTOM-AWT related native tests

--- integration-tests/mtom-awt/src/test/java/io/quarkiverse/cxf/it/ws/mtom/awt/server/MtomAwtIT.java
+++ integration-tests/mtom-awt/src/test/java/io/quarkiverse/cxf/it/ws/mtom/awt/server/MtomAwtIT.java
@@ -1,21 +1,16 @@
 package io.quarkiverse.cxf.it.ws.mtom.awt.server;
 
-import java.io.IOException;
-
-import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
+
+// Native test fails on Mac OS with:
+// java.lang.UnsatisfiedLinkError: no awt in java.library.path
+// https://github.com/oracle/graal/issues/4124
+@DisabledOnOs({ OS.MAC })
 class MtomAwtIT extends MtomAwtTest {
 
-    @Override
-    public void uploadDownloadMtom() throws IOException {
-        // Native test fails on Mac OS with:
-        // java.lang.UnsatisfiedLinkError: no awt in java.library.path
-        // https://github.com/oracle/graal/issues/4124
-        // Only execute this test on Linux for now
-        Assumptions.assumeTrue(System.getProperty("os.name").contains("nux"));
-        super.uploadDownloadMtom();
-    }
 }
