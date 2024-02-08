2f872c3e21303d1763573bd51a7487f74c2668fb
shreya kajbaje <51292172+shreyakajbaje@users.noreply.github.com>
2024-02-08 15:54:17 +0530

    Update CxfClientTestResource.java

--- integration-tests/client/src/test/java/io/quarkiverse/cxf/client/it/CxfClientTestResource.java
+++ integration-tests/client/src/test/java/io/quarkiverse/cxf/client/it/CxfClientTestResource.java
@@ -39,7 +39,7 @@ public class CxfClientTestResource implements QuarkusTestResourceLifecycleManage
 
     @Override
     public Map<String, String> start() {
-
+        String remoteHost = System.getProperty("remote.host");
         network = Network.newNetwork();
 
         final String PROXY_USER = "proxyuser";
@@ -48,49 +48,92 @@ public class CxfClientTestResource implements QuarkusTestResourceLifecycleManage
         final String BASIC_AUTH_USER = "tester";
         final String BASIC_AUTH_PASSWORD = UUID.randomUUID().toString();
 
-        try {
-            calculatorContainer = new GenericContainer<>("quay.io/l2x6/calculator-ws:1.3")
-                    .withExposedPorts(SOAP_SERVICE_PORT)
-                    .withNetworkAliases(SOAP_SERVICE_HOST)
-                    .withNetwork(network)
-                    .withEnv("BASIC_AUTH_USER", BASIC_AUTH_USER)
-                    .withEnv("BASIC_AUTH_PASSWORD", BASIC_AUTH_PASSWORD)
-                    .waitingFor(Wait.forHttp("/calculator-ws/CalculatorService?wsdl"));
+        if (remoteHost == null) {
+            try {
+                calculatorContainer = new GenericContainer<>("quay.io/l2x6/calculator-ws:1.3")
+                        .withExposedPorts(SOAP_SERVICE_PORT)
+                        .withNetworkAliases(SOAP_SERVICE_HOST)
+                        .withNetwork(network)
+                        .withEnv("BASIC_AUTH_USER", BASIC_AUTH_USER)
+                        .withEnv("BASIC_AUTH_PASSWORD", BASIC_AUTH_PASSWORD)
+                        .waitingFor(Wait.forHttp("/calculator-ws/CalculatorService?wsdl"));
+    
+                calculatorContainer.start();
+    
+                skewedCalculatorContainer = new GenericContainer<>("quay.io/l2x6/calculator-ws:1.3")
+                        .withEnv("ADD_TO_RESULT", "100")
+                        .withExposedPorts(SOAP_SERVICE_PORT)
+                        .waitingFor(Wait.forHttp("/calculator-ws/CalculatorService?wsdl"));
+    
+                skewedCalculatorContainer.start();
+    
+                proxy = new GenericContainer<>("mitmproxy/mitmproxy:10.0.0")
+                        .withCommand("mitmdump", "--set", "proxyauth=" + PROXY_USER + ":" + PROXY_PASSWORD)
+                        .withExposedPorts(PROXY_PORT)
+                        .withNetwork(network)
+                        .waitingFor(Wait.forListeningPort());
+                proxy.start();
+    
+                final Map<String, String> props = new LinkedHashMap<>();
+                props.put("cxf.it.calculator.baseUri",
+                        "http://" + calculatorContainer.getHost() + ":" + calculatorContainer.getMappedPort(SOAP_SERVICE_PORT));
+                props.put("cxf.it.skewed-calculator.baseUri",
+                        "http://" + skewedCalculatorContainer.getHost() + ":"
+                                + skewedCalculatorContainer.getMappedPort(SOAP_SERVICE_PORT));
+                props.put("cxf.it.calculator.auth.basic.user", BASIC_AUTH_USER);
+                props.put("cxf.it.calculator.auth.basic.password", BASIC_AUTH_PASSWORD);
+    
+                props.put(
+                        "cxf.it.calculator.hostNameUri",
+                        "http://" + SOAP_SERVICE_HOST + ":" + SOAP_SERVICE_PORT);
+    
+                props.put("cxf.it.calculator.proxy.host", proxy.getHost());
+                props.put("cxf.it.calculator.proxy.port", String.valueOf(proxy.getMappedPort(PROXY_PORT)));
+                props.put("cxf.it.calculator.proxy.user", PROXY_USER);
+                props.put("cxf.it.calculator.proxy.password", PROXY_PASSWORD);
+            }else{
+                 try {
+                calculatorContainer = new GenericContainer<>("quay.io/l2x6/calculator-ws:1.3")
+                        .withExposedPorts(SOAP_SERVICE_PORT)
+                        .withNetworkAliases(SOAP_SERVICE_HOST)
+                        .withNetwork(network)
+                        .withEnv("BASIC_AUTH_USER", BASIC_AUTH_USER)
+                        .withEnv("BASIC_AUTH_PASSWORD", BASIC_AUTH_PASSWORD)
+                        .waitingFor(Wait.forHttp("/calculator-ws/CalculatorService?wsdl"));
+    
+                calculatorContainer.start();
+    
+                skewedCalculatorContainer = new GenericContainer<>("quay.io/l2x6/calculator-ws:1.3")
+                        .withEnv("ADD_TO_RESULT", "100")
+                        .withExposedPorts(SOAP_SERVICE_PORT)
+                        .waitingFor(Wait.forHttp("/calculator-ws/CalculatorService?wsdl"));
+    
+                skewedCalculatorContainer.start();
 
-            calculatorContainer.start();
-
-            skewedCalculatorContainer = new GenericContainer<>("quay.io/l2x6/calculator-ws:1.3")
-                    .withEnv("ADD_TO_RESULT", "100")
-                    .withExposedPorts(SOAP_SERVICE_PORT)
-                    .waitingFor(Wait.forHttp("/calculator-ws/CalculatorService?wsdl"));
-
-            skewedCalculatorContainer.start();
-
-            proxy = new GenericContainer<>("mitmproxy/mitmproxy:10.0.0")
-                    .withCommand("mitmdump", "--set", "proxyauth=" + PROXY_USER + ":" + PROXY_PASSWORD)
-                    .withExposedPorts(PROXY_PORT)
-                    .withNetwork(network)
-                    .waitingFor(Wait.forListeningPort());
-            proxy.start();
-
-            final Map<String, String> props = new LinkedHashMap<>();
-            props.put("cxf.it.calculator.baseUri",
-                    "http://" + calculatorContainer.getHost() + ":" + calculatorContainer.getMappedPort(SOAP_SERVICE_PORT));
-            props.put("cxf.it.skewed-calculator.baseUri",
-                    "http://" + skewedCalculatorContainer.getHost() + ":"
-                            + skewedCalculatorContainer.getMappedPort(SOAP_SERVICE_PORT));
-            props.put("cxf.it.calculator.auth.basic.user", BASIC_AUTH_USER);
-            props.put("cxf.it.calculator.auth.basic.password", BASIC_AUTH_PASSWORD);
-
-            props.put(
-                    "cxf.it.calculator.hostNameUri",
-                    "http://" + SOAP_SERVICE_HOST + ":" + SOAP_SERVICE_PORT);
-
-            props.put("cxf.it.calculator.proxy.host", proxy.getHost());
-            props.put("cxf.it.calculator.proxy.port", String.valueOf(proxy.getMappedPort(PROXY_PORT)));
-            props.put("cxf.it.calculator.proxy.user", PROXY_USER);
-            props.put("cxf.it.calculator.proxy.password", PROXY_PASSWORD);
-
+                // proxy = new GenericContainer<>("mitmproxy/mitmproxy:10.0.0")
+                //         .withCommand("mitmdump", "--set", "proxyauth=" + PROXY_USER + ":" + PROXY_PASSWORD)
+                //         .withExposedPorts(PROXY_PORT)
+                //         .withNetwork(network)
+                //         .waitingFor(Wait.forListeningPort());
+                // proxy.start();
+                final Map<String, String> props = new LinkedHashMap<>();
+                props.put("cxf.it.calculator.baseUri",
+                        "http://" + calculatorContainer.getHost() + ":" + calculatorContainer.getMappedPort(SOAP_SERVICE_PORT));
+                props.put("cxf.it.skewed-calculator.baseUri",
+                        "http://" + skewedCalculatorContainer.getHost() + ":"
+                                + skewedCalculatorContainer.getMappedPort(SOAP_SERVICE_PORT));
+                props.put("cxf.it.calculator.auth.basic.user", BASIC_AUTH_USER);
+                props.put("cxf.it.calculator.auth.basic.password", BASIC_AUTH_PASSWORD);
+    
+                props.put(
+                        "cxf.it.calculator.hostNameUri",
+                        "http://" + SOAP_SERVICE_HOST + ":" + SOAP_SERVICE_PORT);
+    
+                props.put("cxf.it.calculator.proxy.host", remotehost);
+                props.put("cxf.it.calculator.proxy.port", String.valueOf(TINY_PROXY_PORT));
+                props.put("cxf.it.calculator.proxy.user", "proxyuser");
+                props.put("cxf.it.calculator.proxy.password", "proxyuser");
+            }
             return props;
         } catch (Exception e) {
             throw new RuntimeException(e);
